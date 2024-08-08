ae43131900732a478cdd46e961a1c226f9c594eb
Mizuki Kobayashi <umizu.51591@gmail.com>
2024-08-08 21:56:39 +0900

    問題3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    PhoneNumberValidator phoneNumberValidator = new PhoneNumberValidator();
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("携帯電話番号を入力してください:");
+    String phoneNumber = scanner.nextLine();
+
+    if (phoneNumberValidator.isValidPhoneNumber(phoneNumber)) {
+      System.out.println(phoneNumber + " は有効な携帯電話番号です。");
+    } else {
+      System.out.println(phoneNumber + " は無効な携帯電話番号です。");
+    }
+    scanner.close();
   }
 }
